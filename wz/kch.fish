8947b6b95e53653be92caac8ca1f7eee29540e83
wezbest <110793713+wezbest@users.noreply.github.com>
2024-06-26 09:10:54 +0000

    .

---
+++ wz/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspace/h1/wb/pnp.fish
+/workspace/h1/wb/fixrs.fish
+/workspace/h1/wb/st.sh
+/workspace/h1/wb/aryt.fish
+/workspace/h1/wb/tgpt.fish
+/workspace/h1/wb/bun.fish
+/workspace/h1/wb/tmuxx.fish
+
+
