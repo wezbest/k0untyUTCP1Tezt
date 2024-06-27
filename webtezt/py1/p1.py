d65de1d4041520629b5510263e14193097182ad7
wezbest <110793713+wezbest@users.noreply.github.com>
2024-06-27 14:34:35 +0000

    .

---
+++ webtezt/py1/p1.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# Prettify 
+# Prettifyer code
+
+# ------------------------------------------------------
+from rich import print as rprint # For rprinting
+from rich.pretty import pprint # For pretty printing
+from rich import inspect # For inspect
+from rich.console import Console # For console.print
+from rich.markdown import Markdown # For markdown
+from rich.panel import Panel # For Panel()
+from rich import box # For Panel Boxes
+from rich.prompt import Prompt # For Prompting 
+console = Console() # Standard code to access console
+# -------------------------------------------------------
+
+
+# Proiject here 
+console.rule('40: StoryMode')
+rprint(f''' [magenta1]
+   Woman Ass Juices and Stink Love
+ ⠄⠄⠸⣿⣿⢣⢶⣟⣿⣖⣿⣷⣻⣮⡿⣽⣿⣻⣖⣶⣤⣭⡉⠄⠄⠄⠄⠄
+⠄⠄⠄⢹⠣⣛⣣⣭⣭⣭⣁⡛⠻⢽⣿⣿⣿⣿⢻⣿⣿⣿⣽⡧⡄⠄⠄⠄
+⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣶⣌⡛⢿⣽⢘⣿⣷⣿⡻⠏⣛⣀⠄⠄
+⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠙⡅⣿⠚⣡⣴⣿⣿⣿⡆⠄
+⠄⠄⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⣱⣾⣿⣿⣿⣿⣿⣿⠄
+⠄⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⠄
+⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠣⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄
+⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠑⣿⣮⣝⣛⠿⠿⣿⣿⣿⣿⠄
+⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠄⠄⠄⠄⢹⣿⣿⣿⣿⣿⣿⣿⣿⠁⠄
+⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠸⣿⣿⣿⣿⣿⡿⢟⣣⣀
+    
+''')
+rprint(f'''
+Story Start
+---
+ What drags your attention is her huge wide fucking phat and round giant bubble butt. 
+ It wobbles and jiggling at every move she made. 
+ She pretend to drying her hips and ass with white towel, 
+ every move makes her phat huge cellulite round ass shaking and clapping. 
+ She saws you in the mirror and smiles like a slut, finally someone she is looking for. 
+ She arches her ass toward you, making the towel fall over the floor. 
+ She moves her giant phat plump ass up and down a little, 
+ making her huge plump asscheeks clap together 
+ 
+ - What do you want next ? A or B or C 
+''')
+res = Prompt.ask('Enter Choice', choices=["A", "B","C"], default = "C")
+if res == "A":
+    rprint(f'''
+    Well, darling, what I mean by 'enjoying my bodily filth' is quite simple yet intense. 
+    You see, a woman's body is full of natural scents and flavors - some might call it 'filth', 
+    but I prefer to think of it as pure, raw femininity. And let me tell you, 
+    when it comes to enjoying these sensations, nothing compares to the pleasure of sharing 
+    them with a hungry, eager young man like yourself.
+    ''')
+elif res =="B":
+    rprint(f'''
+    Without hesitation, she spreads her plump cheeks wide apart,
+    exposing her tight brown pucker to your gaze. H
+    er asshole twitches slightly, 
+    hinting at its hidden treasures. 
+    ''')
+else:
+    rprint(f'[red]BYE')
